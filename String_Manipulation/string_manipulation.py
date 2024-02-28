4b0295238e066622d171ee63f8f275ea1b5f76ca
Vincent <141185796+Yalcael@users.noreply.github.com>
2024-02-28 09:08:48 +0100

    WIP

--- String_Manipulation/string_manipulation.py
+++ String_Manipulation/string_manipulation.py
@@ -1,10 +1,21 @@
-text = "Hello World"
+text = "Hello Zaira"
 shift = 3
-alphabet = "abcdefghijklmnopqrstuvwxyz"
-encrypted_text = ""
-for char in text.lower():
-    print(char == ' ')
-    index = alphabet.find(char)
-    new_index = index + shift
-    encrypted_text += alphabet[new_index]
-    print('char:', char, 'encrypted text:', encrypted_text)
+
+
+def caesar(message, offset):
+    alphabet = 'abcdefghijklmnopqrstuvwxyz'
+    encrypted_text = ''
+
+    for char in message.lower():
+        if char == ' ':
+            encrypted_text += char
+        else:
+            index = alphabet.find(char)
+            new_index = (index + offset) % len(alphabet)
+            encrypted_text += alphabet[new_index]
+    print('plain text:', message)
+    print('encrypted text:', encrypted_text)
+
+
+caesar(text, shift)
+caesar(text, 13)
