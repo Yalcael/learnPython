5bf254338b9b52a0e498e6a5353d57daf447bbc6
Vincent <141185796+Yalcael@users.noreply.github.com>
2024-02-28 07:14:00 +0100

    WIP

--- String_Manipulation/string_manipulation.py
+++ String_Manipulation/string_manipulation.py
@@ -1,1 +1,10 @@
-number = 5
+text = "Hello World"
+shift = 3
+alphabet = "abcdefghijklmnopqrstuvwxyz"
+encrypted_text = ""
+for char in text.lower():
+    print(char == ' ')
+    index = alphabet.find(char)
+    new_index = index + shift
+    encrypted_text += alphabet[new_index]
+    print('char:', char, 'encrypted text:', encrypted_text)
