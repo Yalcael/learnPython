2d3be2f4288439cf076306e55dafa159ae6715ec
Vincent <141185796+Yalcael@users.noreply.github.com>
2024-02-29 01:17:43 +0100

    WIP

--- String_Manipulation/string_manipulation.py
+++ String_Manipulation/string_manipulation.py
@@ -1,21 +1,37 @@
-text = "Hello Zaira"
-shift = 3
+text = "mrttaqrhknsw ih puggrur"
+custom_key = "happycoding"
 
 
-def caesar(message, offset):
-    alphabet = 'abcdefghijklmnopqrstuvwxyz'
-    encrypted_text = ''
+def vigenere(message, key, direction=1):
+    key_index = 0
+    alphabet = "abcdefghijklmnopqrstuvwxyz"
+    final_message = ""
 
     for char in message.lower():
-        if char == ' ':
-            encrypted_text += char
+        # Append any non-letter character to the message
+        if not char.isalpha():
+            final_message += char
         else:
+            # Find the right key character to encode/decode
+            key_char = key[key_index % len(key)]
+            key_index += 1
+            # Define the offset and the encrypted/decrypted letter
+            offset = alphabet.index(key_char)
             index = alphabet.find(char)
-            new_index = (index + offset) % len(alphabet)
-            encrypted_text += alphabet[new_index]
-    print('plain text:', message)
-    print('encrypted text:', encrypted_text)
+            new_index = (index + offset * direction) % len(alphabet)
+            final_message += alphabet[new_index]
+    return final_message
 
 
-caesar(text, shift)
-caesar(text, 13)
+def encrypt(message, key):
+    return vigenere(message, key)
+
+
+def decrypt(message, key):
+    return vigenere(message, key, -1)
+
+
+print(f"\nEncrypted text: {text}")
+print(f"Key: {custom_key}")
+decryption = decrypt(text, custom_key)
+print(f"\nDecrypted text: {decryption}\n")
