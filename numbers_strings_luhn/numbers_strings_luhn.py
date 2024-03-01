eed302183df2086bfba484cb731dc96c4272b8cb
Vincent <141185796+Yalcael@users.noreply.github.com>
2024-03-01 11:19:32 +0100

    WIP

--- numbers_strings_luhn/numbers_strings_luhn.py
+++ numbers_strings_luhn/numbers_strings_luhn.py
@@ -5,17 +5,26 @@ def verify_card_number(card_number):
 
     for digit in odd_digits:
         sum_of_odd_digits += int(digit)
+
     sum_of_even_digits = 0
     even_digits = card_number_reversed[1::2]
     for digit in even_digits:
-        print(digit)
+        number = int(digit) * 2
+        if number >= 10:
+            number = (number // 10) + (number % 10)
+        sum_of_even_digits += number
+    total = sum_of_odd_digits + sum_of_even_digits
+    return total % 10 == 0
 
 
 def main():
-    card_number = '4111-1111-4555-1142'
-    card_translation = str.maketrans({'-': '', ' ': ''})
+    card_number = "4111-1111-4555-1142"
+    card_translation = str.maketrans({"-": "", " ": ""})
     translated_card_number = card_number.translate(card_translation)
-    verify_card_number(translated_card_number)
+    if verify_card_number(translated_card_number):
+        print("VALID!")
+    else:
+        print("INVALID!")
 
 
 main()
