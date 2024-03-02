4c35e5a82140af352e5a5f91cfb4c3629fef1c0d
Vincent <141185796+Yalcael@users.noreply.github.com>
2024-03-02 09:09:22 +0100

    WIP

--- lambda/lambda.py
+++ lambda/lambda.py
@@ -1,5 +1,14 @@
 def add_expense(expenses, amount, category):
-    expenses.append()
+    expenses.append({"amount": amount, "category": category})
+
+
+def print_expenses(expenses):
+    for expense in expenses:
+        print(f'Amount: {expense["amount"]}, Category: {expense["category"]}')
+
+
+def total_expenses(expenses):
+    pass
 
 
 expenses = []
