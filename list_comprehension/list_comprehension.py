f34f0f8337723fc6688e32bd051d968f93f0ad00
Vincent <141185796+Yalcael@users.noreply.github.com>
2024-03-03 20:41:14 +0100

    WIP

--- list_comprehension/list_comprehension.py
+++ list_comprehension/list_comprehension.py
@@ -1,4 +1,10 @@
 def convert_to_snake_case(pascal_or_camel_cased_string):
     snake_cased_char_list = []
     for char in pascal_or_camel_cased_string:
-        pass
+        if char.isupper():
+            converted_character = "_" + char.lower()
+            snake_cased_char_list.append(converted_character)
+        else:
+            snake_cased_char_list.append(char)
+    snake_cased_string = "".join(snake_cased_char_list)
+    clean_snake_cased_string = snake_cased_string.strip("_")
