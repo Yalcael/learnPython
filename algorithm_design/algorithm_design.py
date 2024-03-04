ce508c8c298c368159e99e5cb78c9e4f89fe94f3
Vincent <141185796+Yalcael@users.noreply.github.com>
2024-03-04 12:57:15 +0100

    WIP

--- algorithm_design/algorithm_design.py
+++ algorithm_design/algorithm_design.py
@@ -1,1 +1,16 @@
-copper = {"species": "guinea pig", "age": 2}
+my_graph = {
+    "A": [("B", 3), ("D", 1)],
+    "B": [("A", 3), ("C", 4)],
+    "C": [("B", 4), ("D", 7)],
+    "D": [("A", 1), ("C", 7)],
+}
+
+
+def shortest_path(graph, start):
+    unvisited = list(graph)
+    distances = {}
+    paths = {key: [] for key in graph}
+    print(f"Unvisited: {unvisited}\nDistances: {distances}")
+
+
+shortest_path(my_graph, "A")
