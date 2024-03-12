57a90ed41b242b7fcd8604c5221b4326484ddc20
Vincent <141185796+Yalcael@users.noreply.github.com>
2024-03-12 06:17:40 +0100

    WIP

---
+++ dataclasses/function_vs_classes.py
@@ -0,0 +1,11 @@
+# Functions take input arguments, then do something, and they return a result.
+# Or you can take that result and pas it to other functions.
+# Use functions when your code is action focused and use classes when your code is state focused.
+
+# What's nice about Python it's we have a huge flexibility, try to use a careful combination of both!
+
+# Basically, if the programme that you're writing is more focused with the flow , like processing data in sequence,
+# Then using a more functional approach is going to be better, and it's also going to make it easier to write tests.
+# If it's really more focused on state, like the bank accounts example, or you have collections of objects that need to be structured in some way,
+# And you have a few methods that modify those objects that with the class based approach will typically be better.
+# USE DATACLASS IF YOU ARE DOING CLASSES
