7c25d2543af3505bdd95d2409789719b9d95c00b
Vincent <141185796+Yalcael@users.noreply.github.com>
2024-03-25 06:28:19 +0100

    WIP

--- simple adder/simpler_adder.py
+++ simple adder/simpler_adder.py
@@ -6,3 +6,17 @@ b: str = input("Enter your value for 'b': ")
 print('---------------------------------')
 
 print("The result is:", int(a) + int(b))
+
+
+number: list[int] = [1, 2, 3, 4]
+print(number)
+number.append(5)
+print(number)
+number.remove(5)
+print(number)
+number.pop(1)
+print(number)
+number.insert(1, 2)
+print(number)
+number[0] = 0
+print(number)
