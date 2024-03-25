eb1c34fac805de2a6bec4eb86c0be580fc64d8fd
Vincent <141185796+Yalcael@users.noreply.github.com>
2024-03-25 06:10:08 +0100

    WIP

--- recursion/recursion.py
+++ recursion/recursion.py
@@ -1,1 +1,26 @@
-rods = {"A": [], "B": [], "C": []}
+NUMBER_OF_DISKS = 3
+number_of_moves = 2**NUMBER_OF_DISKS - 1
+rods = {
+    'A': list(range(NUMBER_OF_DISKS, 0, -1)),
+    'B': [],
+    'C': []
+}
+
+
+def move(n, source, auxiliary, target):
+    # display starting configuration
+    print(rods)
+    for i in range(number_of_moves):
+        remainder = (i + 1) % 3
+        if remainder == 1:
+            print(f'Move {i + 1} allowed between {source} and {target}')
+            forward = False
+            if not rods[target]:
+                forward = True
+        elif remainder == 2:
+            print(f'Move {i + 1} allowed between {source} and {auxiliary}')
+        elif remainder == 0:
+            print(f'Move {i + 1} allowed between {auxiliary} and {target}')
+
+    # initiate call from source A to target C with auxiliary B
+    move(NUMBER_OF_DISKS, 'A', 'B', 'C')
---
+++ simple adder/simpler_adder.py
@@ -0,0 +1,8 @@
+print("Welcome to your simple adder!")
+print('---------------------------------')
+
+a: str = input("Enter your value for 'a': ")
+b: str = input("Enter your value for 'b': ")
+print('---------------------------------')
+
+print("The result is:", int(a) + int(b))
