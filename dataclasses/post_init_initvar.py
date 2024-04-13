dca60570e4092ec698930cfe09f16a7a26d6c2a4
Vincent <141185796+Yalcael@users.noreply.github.com>
2024-04-13 10:59:59 +0200

    WIP

--- dataclasses/post_init_initvar.py
+++ dataclasses/post_init_initvar.py
@@ -1,4 +1,4 @@
-from dataclasses import dataclass, field
+from dataclasses import dataclass, field, InitVar
 
 
 @dataclass
@@ -6,9 +6,12 @@ class Fruit:
     name: str
     grams: float
     cost_per_kg: float
+    is_rare: InitVar[bool | None] = None
     total_price: float = field(init=False)
 
-    def __post_init__(self) -> None:
+    def __post_init__(self, is_rare: bool | None) -> None:
+        if is_rare:
+            self.cost_per_kg *= 2
         self.total_price = (self.grams / 1000) * self.cost_per_kg
 
     def describe(self) -> None:
@@ -18,10 +21,13 @@ class Fruit:
 def main() -> None:
     apple: Fruit = Fruit("Apple", 1500, 4)
     orange: Fruit = Fruit("Orange", 2500, 8)
+    passion: Fruit = Fruit("Passion", 100, 50, is_rare=True)
     print(apple)
     print(orange)
+    print(passion)
     apple.describe()
     orange.describe()
+    passion.describe()
 
 
 if __name__ == '__main__':
